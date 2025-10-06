42c163df8b7ee0fe862b66690ba8052f969d4ce1
Alexader1309 <alexanderperez4893@gmail.com>
2025-10-06 12:10:21 -0600

    Implement code changes to enhance functionality and improve performance

--- views/templates/header.php
+++ views/templates/header.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="es">
-
 <?php echo $this->getHeadContent(); ?>
 
 <body class="no-sidebar <?php echo $this->getBodyClass(); ?>" <?php echo $this->getBodyAttributes(); ?>>
@@ -9,25 +8,32 @@
 	<nav class="navbar navbar-expand-lg bg-dark">
 		<div class="container">
 			<a class="navbar-brand text-white" href="#">
-				<img src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="Bootstrap" width="30" height="24">
-				Rifas La Paz
+				<img src="/assets/images/logo.png" alt="Rifas La Paz" width="110" height="55">
 			</a>
-			<button class="navbar-toggler bg-body" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+			<button class=" navbar-toggler bg-body" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
 				<span class="navbar-toggler-icon"></span>
 			</button>
 			<div class="collapse navbar-collapse" id="navbarNav">
-				<ul class="navbar-nav">
+				<ul class="navbar-nav ms-auto align-items-center">
 					<li class="nav-item">
-						<a class="nav-link active text-white" aria-current="page" href="#">Home</a>
+						<a class="nav-link text-white" href="#">
+							<i class="bi bi-info-circle-fill"></i> Sobre Nosotros
+						</a>
+					</li>
+					<li class="nav-item ms-3">
+						<a class="nav-link text-white" href="#" title="Facebook">
+							<i class="bi bi-facebook fs-5"></i>
+						</a>
 					</li>
 					<li class="nav-item">
-						<a class="nav-link text-white" href="#">Features</a>
+						<a class="nav-link text-white" href="#" title="Instagram">
+							<i class="bi bi-instagram fs-5"></i>
+						</a>
 					</li>
 					<li class="nav-item">
-						<a class="nav-link text-white" href="#">Pricing</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link disabled text-white" aria-disabled="true">Disabled</a>
+						<a class="nav-link text-white" href="#" title="WhatsApp">
+							<i class="bi bi-whatsapp fs-5"></i>
+						</a>
 					</li>
 				</ul>
 			</div>
